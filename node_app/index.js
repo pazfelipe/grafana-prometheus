466b38225ede9bc7efa0adef1e08a271ea4fa542
Felipe Paz <xxfelipepaz@gmail.com>
2024-07-06 08:31:47 -0300

    refactor: import register and metrics from prom-client

--- node_app/index.js
+++ node_app/index.js
@@ -3,6 +3,12 @@ const http = require("http");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const {
+  register,
+  httpRequestDurationMicroseconds,
+  httpRequestCounter,
+  errorCounter
+} = require('./promClient');
 
 const logger = require('./logger');
 const route = require('./routes');
@@ -17,10 +23,30 @@ app.use(morgan('combined', {
   stream: {write: (message) => logger.info(message.trim())}
 }));
 
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    const labels = {method: req.method, route: req.route ? req.route.path : 'unknown', status_code: res.statusCode};
+    end(labels);
+    httpRequestCounter.inc(labels);
+  });
+  next();
+});
+
 
 app.use(route);
 
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
 app.use((err, req, res, next) => {
+  errorCounter.inc({error_type: err.name});
   logger.error(err.message, {stack: err.stack});
   res.status(500).send({error: 'Something went wrong!'});
 });
@@ -34,9 +60,11 @@ server.listen(PORT, () => {
 });
 
 process.on('unhandledRejection', (reason, promise) => {
+  errorCounter.inc({ error_type: 'unhandledRejection' });
   logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
 process.on('uncaughtException', (error) => {
+  errorCounter.inc({ error_type: 'uncaughtException' });
   logger.error('Uncaught Exception:', error);
 });
